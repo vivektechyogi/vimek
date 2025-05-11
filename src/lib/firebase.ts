beb9c140d1c021fa495444f61890a87a31e14b5c
Vivek <iamviveklalan@gmail.com>
2025-05-11 19:18:42 +0530

    update firebase config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,13 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID, // Optional
+  apiKey: "AIzaSyD6KK7FCrwqUcPphEjsgRWQCZr5qOc2pmc",
+  authDomain: "vimek-8728d.firebaseapp.com",
+  projectId: "vimek-8728d",
+  storageBucket: "vimek-8728d.firebasestorage.app",
+  messagingSenderId: "264585120007",
+  appId: "1:264585120007:web:68f78cf3e4767e27995da2",
+  measurementId: "G-RD772R7S40"
 };
 
 // Initialize Firebase
